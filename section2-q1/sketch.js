308f1ff9024754c976126a29b5ba3f7dd01f5709
Takumu39 <2170521h@stu.kobe-u.ac.jp>
2022-10-11 11:15:45 +0900

    section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,15 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
+  createCanvas(100,100);
+  background(196);
+  let r;
+  for(let r =5; r < 10; r + 5){
+    ellipse(50,50,r)
+    if( r<100){
+      stroke(255,0,0);
+    }
+    if( r<51)
+      stroke(0,0,255)
+    }
     // BLANK[1]
   }
-}
