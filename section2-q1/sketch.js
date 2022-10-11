7afc10863fa6f9e594c69f5b054a13a065add96a
Takumu39 <2170521h@stu.kobe-u.ac.jp>
2022-10-11 12:12:16 +0900

    Update sketch.js

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,15 +1,14 @@
 // 小手調べ
 function setup(){
-  createCanvas(100,100);
-  background(196);
-  let r;
-  for(let r =5; r < 10; r + 5){
-    ellipse(50,50,r)
-    if( r<100){
+  nofill();
+  
+  for(let i =0; i < 10; i++){
+    if( i<5){
+      stroke(0,0,255);
+    }
+    else{
       stroke(255,0,0);
     }
-    if( r<51)
-      stroke(0,0,255)
-    }
-    // BLANK[1]
+    ellipse(50,50,i*10+10);
   }
+}
